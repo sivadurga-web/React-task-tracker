cf30a4a37b9ad3494742ec947b69d9fa3942d6d6
siva durga <57574795+sivadurga-web@users.noreply.github.com>
2021-07-26 23:12:34 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter as Router} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
     <App />
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
